35a0e0e5e6d2ca54a1118a9b5622ef443d645231
Vytautas Donela <donela.vytautas@gmail.com>
2020-02-28 04:12:38 +0200

    Test Commitas

---
+++ ObjektinioND/ObjektinioND/Ismatavimai.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektinioND
+{
+    public class Ismatavimai
+    {
+        public int Ilgis { get; set; }
+        public int Plotis { get; set; }
+        public int Aukstis { get; set; }
+    }
+}
